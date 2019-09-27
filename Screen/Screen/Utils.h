654c833afb833d82a1e2ecec173c6c2be97402b5
Beomjoo Seo <beomjoo90@gmail.com>
2019-09-27 14:56:47 +0900

    stl vector 사용법
    - Input Class 등록
    - Screen 기능 변경
    - iterator 사용법 연습
    - for each 사용법 학습
    - auto 키워드 공부

--- Screen/Screen/Utils.h
+++ Screen/Screen/Utils.h
@@ -1,7 +1,9 @@
 #ifndef UTILS_H_
 #define UTILS_H_
-
+#include <cstdio>
 #include <Windows.h>
+
+using namespace std;
 
 struct Position {
 	int x;
@@ -9,17 +11,66 @@ struct Position {
 	Position(int x = 0, int y = 0) : x(x), y(y) {}
 };
 
-class Borland {
+class Input {
+	static INPUT_RECORD InputRecord;
+	static DWORD Events;
+	static bool evaluated;
+	static bool gotEvent;
+	static Position mousePosition;
 
 public:
-	static void initialize()
+
+	static void EndOfFrame()
 	{
+		evaluated = false;
+		gotEvent = false;
+	}
+
+	static void Initialize()
+	{
+
 		CONSOLE_CURSOR_INFO cci;
 		cci.dwSize = 25;
 		cci.bVisible = FALSE;
 		SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cci);
 		SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), ENABLE_PROCESSED_INPUT | ENABLE_MOUSE_INPUT);
 	}
+	static bool GetMouseEvent(Position& pos) {
+
+		if (evaluated == true) {
+			if (gotEvent == true) {
+				pos = mousePosition;
+				return true;
+			}
+			return false;
+		}
+		evaluated = true;
+		ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &InputRecord, 1, &Events);
+		if (InputRecord.EventType == MOUSE_EVENT) {
+			if (InputRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
+				COORD coord;
+				coord.X = InputRecord.Event.MouseEvent.dwMousePosition.X;
+				coord.Y = InputRecord.Event.MouseEvent.dwMousePosition.Y;
+				SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+				mousePosition.x = InputRecord.Event.MouseEvent.dwMousePosition.X;
+				mousePosition.y = InputRecord.Event.MouseEvent.dwMousePosition.Y;
+				gotEvent = true;
+			}
+		}
+		return gotEvent;
+	}
+};
+INPUT_RECORD Input::InputRecord;
+DWORD Input::Events;
+
+bool Input::evaluated = false;
+bool Input::gotEvent = false;
+Position Input::mousePosition{ -1, -1 };
+
+class Borland {
+
+public:
+	
 
 	static int wherex()
 	{
@@ -47,5 +98,61 @@ public:
 		gotoxy( pos.x, pos.y);
 	}
 };
+
+class Screen {
+	int width;
+	int height;
+	char* canvas;
+
+	static Screen* instance;
+	Screen(int width = 70, int height = 25)
+		: width(width), height(height),
+		canvas(new char[(width + 1)*height])
+
+	{
+		Input::Initialize();
+	}
+public:
+	static Screen& getInstance() {
+		if (instance == nullptr) {
+			instance = new Screen();
+		}
+		return *instance;
+	}
+
+	~Screen() {
+		if (instance) {
+			delete[] canvas;
+			instance = nullptr;
+		}
+	}
+
+	void draw(const char* shape, int w, int h, const Position& pos)
+	{
+		if (!shape) return;
+		for (int i = 0; i < h; i++)
+		{
+			strncpy(&canvas[pos.x + (pos.y + i)*(width + 1)], &shape[i*w], w);
+		}
+	}
+
+	void render()
+	{
+		for (int i = 0; i < height; i++)
+			canvas[width + i * (width + 1)] = '\n';
+		canvas[width + (height - 1) * (width + 1)] = '\0';
+		Borland::gotoxy(0, 0);
+		cout << canvas;
+	}
+
+	void clear()
+	{
+		memset(canvas, ' ', (width + 1)*height);
+		canvas[width + (height - 1)*(width + 1)] = '\0';
+	}
+
+};
+
+Screen* Screen::instance = nullptr;
 
 #endif 
--- Screen/Screen/main.cpp
+++ Screen/Screen/main.cpp
@@ -4,65 +4,10 @@
 #include <iostream>
 #include <conio.h>
 #include <Windows.h>
+#include <vector>
 #include "Utils.h"
 
 using namespace std;
-
-class Screen {
-	int width;
-	int height;
-	char* canvas;
-
-	static Screen* instance;
-	Screen(int width = 70, int height = 25) 
-		: width(width), height(height),
-		canvas(new char[(width+1)*height])
-
-	{
-		Borland::initialize();
-	}
-public:
-	static Screen& getInstance() {
-		if (instance == nullptr) {
-			instance = new Screen();
-		}
-		return *instance;
-	}
-
-	~Screen() {
-		if (instance) {
-			delete[] canvas;
-			instance = nullptr;
-		}
-	}
-
-	void draw(const char* shape, int w, int h, const Position& pos)
-	{
-		if (!shape) return;
-		for (int i = 0; i < h; i++)
-		{
-			strncpy(&canvas[pos.x + (pos.y+i)*(width+1)], &shape[i*w], w);
-		}
-	}
-
-	void render()
-	{
-		for (int i = 0; i < height; i++)
-			canvas[width + i * (width+1)] = '\n';
-		canvas[width + (height-1) * (width + 1)] = '\0';
-		Borland::gotoxy(0, 0);
-		cout << canvas;
-	}
-
-	void clear()
-	{
-		memset(canvas, ' ', (width + 1)*height);
-		canvas[width + (height-1)*(width+1)] = '\0';
-	}
-
-};
-
-Screen* Screen::instance = nullptr;
 
 class GameObject {
 	char* shape;
@@ -71,10 +16,10 @@ class GameObject {
 	Position pos;
 	int direction;
 	Screen& screen;
-
+	Position mousePosition;
 public:
 	GameObject(const char* shape, int width, int height) 
-		: height(height), width(width), shape(nullptr), pos(0, 0), direction(1),
+		: height(height), width(width), shape(nullptr), pos(0, 0), direction(1), mousePosition{ -1,-1 },
 		screen(Screen::getInstance())
 	{
 		if (!shape || strlen(shape) == 0 || width == 0 || height == 0)
@@ -103,24 +48,28 @@ public:
 
 	void setDirection(int direction) { this->direction = direction; }
 
-	virtual void update(const Position& pos) {
+	virtual void update() {
+
+		
+		if (Input::GetMouseEvent(mousePosition) == false) return;
+
 		switch (direction)
 		{
 		case 1:
 		case 2:
-			if (this->pos.x < pos.x) {
+			if (this->pos.x < mousePosition.x) {
 				(this->pos.x)++;
 			}
-			else if (this->pos.x > pos.x) {
+			else if (this->pos.x > mousePosition.x) {
 				(this->pos.x)--;
 			}
 			break;
 		case 3:
 		case 4:
-			if (this->pos.y < pos.y) {
+			if (this->pos.y < mousePosition.y) {
 				(this->pos.y)++;
 			}
-			else if (this->pos.y > pos.y) {
+			else if (this->pos.y > mousePosition.y) {
 				(this->pos.y)--;
 			}
 			break;
@@ -135,9 +84,15 @@ int main()
 	GameObject enemy2("^^^^^00000^^^^^", 5, 3);
 	GameObject enemy("--00--", 2, 3);
 	Screen&	 screen = Screen::getInstance();
-	INPUT_RECORD InputRecord;
-	DWORD Events;
 
+	int a;
+
+	vector<GameObject *> gameObjects;
+
+	gameObjects.push_back(&player);
+	gameObjects.push_back(&enemy);
+	gameObjects.push_back(&enemy2);
+	
 	player.setPos(1, 1);
 	enemy.setPos(10, 10);
 	enemy2.setPos(5, 5);
@@ -146,28 +101,19 @@ int main()
 
 	screen.clear(); screen.render();
 	while (true)
-	{
-		
+	{	
+		for (auto it = gameObjects.cbegin(); 
+					it != gameObjects.cend(); it++)
+			(*it)->draw();
 
-		player.draw(); enemy.draw(); enemy2.draw();
 		screen.render();		
 		Sleep(30);
 		screen.clear();
 
-		ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &InputRecord, 1, &Events);
-		if (InputRecord.EventType == MOUSE_EVENT) {
-			if (InputRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
-				COORD coord;
-				coord.X = InputRecord.Event.MouseEvent.dwMousePosition.X;
-				coord.Y = InputRecord.Event.MouseEvent.dwMousePosition.Y;
-				SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-				Position pos;
-				pos.x = InputRecord.Event.MouseEvent.dwMousePosition.X;
-				pos.y = InputRecord.Event.MouseEvent.dwMousePosition.Y;
-				player.update(pos);
-				enemy.update(pos); enemy2.update(pos);
-			}
-		}
+		for (auto obj : gameObjects) obj->update();
+		
+
+		Input::EndOfFrame();
 		
 	}
 

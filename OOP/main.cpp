f0cc1aa64e0c9ae9e8775a6afffba49950f5ee14
beomjoo90 <beomjoo90@gmail.com>
2020-04-06 17:50:45 +0900

    문자열 함수를 사용하여 화면 이동
    memset, strncpy 함수 사용
    디버깅하는 방법도 배움.

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,5 +1,6 @@
 // main.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
 #include <Windows.h>
@@ -14,28 +15,29 @@ void printSpaces(int i)
 		printf(" ");
 }
 
+void draw(char* canvas, int pos, const char* source)
+{
+	strncpy(&canvas[pos], source, strlen(source));
+}
+
 int main()
 {
 	const int maxCount = 80;
 	char canvas[maxCount + 1];
 	int player_pos = 0;
-	char player_shape[6] = "(o_o)";
-	int enemy_pos = 75;
-	char enemy_shape[6] = "(*_*)";
-
-	while (player_pos < maxCount && enemy_pos >= 0)
+	char player_shape[100] = "(o_o)";
+	int enemy_pos = maxCount - strlen("(*______*)");
+	char enemy_shape[100] = "(*______*)";
+	
+	while (player_pos < (maxCount - strlen(player_shape)) && enemy_pos >= 0)
 	{
-		for (int i = 0; i < maxCount; i++)
-			canvas[i] = ' ';
+		memset(canvas, ' ', maxCount);		
 
 		// player 그림을 canvas 공간에 player 위치에 복사
-		for (int i = 0; i < 5; i++)
-			canvas[player_pos + i] = player_shape[i];
+		draw(canvas, player_pos, player_shape);
 		player_pos++;
 
-		for (int i = 0; i < 5; i++)
-			canvas[enemy_pos + i] = enemy_shape[i];
-		// enemy 그림을 canvas 공간에 enemy 위치에 복사
+		draw(canvas, enemy_pos, enemy_shape);
 		enemy_pos--;
 
 		canvas[maxCount] = '\0';

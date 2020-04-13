f86cf5fa16f1a8e4c2fabc0cf57d11e482069110
beomjoo90 <beomjoo90@gmail.com>
2020-04-13 18:21:33 +0900

    4/13 키보드 제어 및 총알 발사 기능 구현
    - conio.h에 선언된 _kbhit(), _getch()를 이용하여 키보드 입력을 통해 움직임 제어
    - 총알 나오는 기능 구현

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -6,6 +6,7 @@
 #endif
 
 #include <iostream>
+#include <conio.h>
 #include <Windows.h>
 #include <ctime>
 
@@ -25,20 +26,20 @@ void clear(char* canvas, int length)
 	canvas[length] = '\0';
 }
 
-
-void draw(char* canvas, int pos, const char* source)
+bool isInside(const char* shape, int pos, int length)
 {
+	return pos <= (length - strlen(shape)) && pos >= 0;
+}
+
+void draw(char* canvas, int pos, const char* source, int length)
+{
+	if (isInside(source, pos, length) == false) return;
 	strncpy(&canvas[pos], source, strlen(source));
 }
 
 void render(const char* canvas, int lastPosition)
 {
 	printf("%s\r", canvas);
-}
-
-bool isInside(char* shape, int pos, int length)
-{
-	return pos <= (length - strlen(shape)) && pos >= 0;
 }
 
 int main()
@@ -49,25 +50,78 @@ int main()
 	char player_shape[maxCount] = "(o_o)";
 	int enemy_pos = 0;
 	char enemy_shape[maxCount] = "(*______*)";
+	int bullet_pos = 0;
+	char bullet_shape[maxCount] = "-->";
+	bool isFired = false;
+	int direction = 0; // 0 : left->right, 1: right->left
 
 	//srand(time(nullptr));
 	player_pos = rand() % (maxCount - strlen(player_shape));
 	enemy_pos = rand() % (maxCount - strlen(enemy_shape));
 	
-	while ( isInside(player_shape, player_pos, maxCount) 
-		&& isInside(enemy_shape, enemy_pos, maxCount) )		
+	while (true)		
 	{
-		clear(canvas, maxCount );
+		clear(canvas, maxCount);
+				
+		// update game objects (player, enemy ...)
+		if (isInside(player_shape, player_pos, maxCount) == false
+			|| isInside(enemy_shape, enemy_pos, maxCount) == false)
+			break; // check game loop termination condition
 
-		draw(canvas, player_pos, player_shape);
-		if (rand() % 2) player_pos++;
-		else player_pos--;
-		
-		draw(canvas, enemy_pos, enemy_shape);
-		if (rand() % 2) enemy_pos++;
-		else enemy_pos--;
-		
+		if (_kbhit()) {
+			int key = _getch();
+			//printf("\n%c %d\n", key, key);
+			switch (key) {
+			case 'a':
+				player_pos--;
+				break;
+			case 'd':
+				player_pos++;
+				break;
+			case ' ':
+				if (isFired == true) 
+					break;
+				isFired = true;
+				bullet_pos = player_pos;
+				if (player_pos < enemy_pos) {
+					bullet_pos += strlen(player_shape)-1;
+					strncpy(bullet_shape, "-->", strlen("-->"));
+					direction = 0;
+				}
+				else {
+					strncpy(bullet_shape, "<--", strlen("<--"));
+					direction = 1;
+				}
+				break;
+			case 'w':
+				enemy_pos++;
+				break;
+			case 's':
+				enemy_pos--;
+				break;
+			}
+		}
+		if (isFired == true) {
+			if (direction == 0)
+				bullet_pos++;
+			else bullet_pos--;
 
+			if ( (direction == 0 && enemy_pos <= bullet_pos)
+				|| (direction == 1 && bullet_pos < enemy_pos+strlen(enemy_shape) ) )
+			{
+				isFired = false;
+			}
+		}
+
+
+		
+		// draw game objects to a canvas (player, enemy ...)
+		draw(canvas, player_pos, player_shape, maxCount);
+		draw(canvas, enemy_pos, enemy_shape, maxCount);
+		if (isFired == true)
+			draw(canvas, bullet_pos, bullet_shape, maxCount);
+		
+		// display canvas to a monitor
 		render(canvas, maxCount);		
 		Sleep(100);
 	}

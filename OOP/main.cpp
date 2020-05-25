49353b9ac6613c88eec90d868dd093298717e2c6
PilGyu-Jung <panther9596@hanmail.net>
2020-05-25 16:02:21 +0900

    sad
    asd

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,221 +1,525 @@
-// main.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
 #ifndef _CRT_SECURE_NO_WARNINGS
+
 #define _CRT_SECURE_NO_WARNINGS
+
 #endif
 
+
+
 #include <iostream>
+
 #include <conio.h>
+
 #include <Windows.h>
+
 #include <ctime>
+
+#include <cstdlib> // include malloc,
+
+
+
 
 
 // commit practice
+
 // 커밋 연습
 
-void printSpaces(int i)
-{
-	for (int j = 0; j < i; j++)
-		printf(" ");
-}
 
-void clear(char* canvas, int length)
-{
-	memset(canvas, ' ', length);
-	canvas[length] = '\0';
-}
 
-void render(const char* canvas, int lastPosition)
-{
-	printf("%s\r", canvas);
-}
+struct Screen {
 
-struct Enemy {
+	int		len;
+
+	char* canvas;
+
+
+
+	Screen(int maxCount = 95)
+
+		: len(maxCount + 1), canvas{ (char*)malloc(sizeof(char) * (maxCount + 1)) }
+
+	{
+
+	}
+
+
+
+	// 소멸자, destructor
+
+	~Screen()
+
+	{
+
+		free((void*)canvas);
+
+		canvas = nullptr;
+
+		len = 0;
+
+	}
+
+
+
+	int length() { return len - 1; }
+
+
+
+	void clear()
+
+	{
+
+		memset(canvas, ' ', len - 1);
+
+		canvas[len - 1] = '\0';
+
+	}
+
+
+
+	void render()
+
+	{
+
+		printf("%s\r", canvas);
+
+	}
+
+
+
+	void draw(const char* shape, int pos)
+
+	{
+
+		strncpy(&canvas[pos], shape, strlen(shape));
+
+	}
+
+
+
+};
+
+
+
+struct GameObject {
+
 	int		pos;
-	char	shape[100];
+
+	char	shape[100]; // 0 ... 99
+
+
+
+	GameObject(int pos, const char* shape)
+
+		: pos(pos)
+
+	{
+
+		setShape(shape);
+
+	}
+
+
+
+	//getter 게터
+
+	int getPos() { return pos; }
+
+	const char* getShape() { return shape; }
+
+
+
+	//setter 세터
+
+	void setPos(int pos) { this->pos = pos; }
+
+	void setShape(const char* shape)
+
+	{
+
+		int len = strlen(shape); // len >= 0
+
+		if (len >= 100) {
+
+			// 0 ... 98
+
+			// 99에는 널문자
+
+			strncpy(this->shape, shape, 100 - 1);
+
+			this->shape[100 - 1] = '\0';
+
+		}
+
+		else
+
+			strcpy(this->shape, shape);
+
+	}
+
+
+
+
+
+	bool isInside(int length)
+
+	{
+
+		return pos <= (length - strlen(shape)) && pos >= 0;
+
+	}
+
+
+
+	void moveRight()
+
+	{
+
+		pos++;
+
+	}
+
+
+
+	void moveLeft()
+
+	{
+
+		pos--;
+
+	}
+
+
+
+	void draw(Screen& screen)
+
+	{
+
+		if (isInside(screen.length()) == false) return;
+
+		screen.draw(shape, pos);
+
+	}
+
+};
+
+
+
+struct Enemy : public GameObject {
+
+
 
 	Enemy(const char* shape, int maxCount)
-	{	
-		strcpy(this->shape, shape);
-		this->pos = rand() % (maxCount - strlen(this->shape));
+
+		: GameObject(rand() % (maxCount - (int)strlen(shape)), shape)
+
+	{
+
 	}
 
-	bool isInside(int length)
-	{
-		return pos <= (length - strlen(shape)) && pos >= 0;
-	}
-
-	void moveRight()
-	{
-		pos++;
-	}
-
-	void moveLeft()
-	{
-		pos--;
-	}
-
-	void draw(char* canvas, int maxCount)
-	{
-		if (isInside(maxCount) == false) return;
-		strncpy(&canvas[pos], shape, strlen(shape));
-	}
 };
 
-struct Bullet {
-	int		pos;
-	char	shape[100];
+
+
+struct Bullet : public GameObject {
+
 	bool	isFired;
+
 	int		direction;
 
-	Bullet(const char* shape)
+
+
+	Bullet(const char* shape = "")
+
+		: GameObject(-1, shape), isFired(false), direction(0)
+
 	{
-		this->pos = 0;
-		strcpy(this->shape, shape);
-		this->isFired = false;
-		this->direction = 0;
+
 	}
 
-	bool isInside(int length)
+
+
+	bool checkFire()
+
 	{
-		return pos <= (length - strlen(shape)) && pos >= 0;
+
+		return isFired == true;
+
 	}
 
-	void moveRight()
+	void setFire()
+
 	{
-		pos++;
+
+		isFired = true;
+
 	}
 
-	void moveLeft()
+	void resetFire()
+
 	{
-		pos--;
+
+		isFired = false;
+
 	}
+
+
+
+	void makeDirectionLeft() { direction = 1; }
+
+	void makeDirectionRight() { direction = 0; }
+
+	bool isDirectionRight() { return direction == 0; }
+
+
+
+
 
 	void update(int enemy_pos, const char* enemy_shape)
-	{
-		if (isFired == false) return;
 
-		if (direction == 0)
+	{
+
+		if (checkFire() == false) return;
+
+
+
+		if (isDirectionRight())
+
 			moveRight();
+
 		else moveLeft();
 
-		if ((direction == 0 && enemy_pos <= pos)
-			|| (direction == 1 && pos < enemy_pos + strlen(enemy_shape)))
+
+
+		int pos = getPos();
+
+		if ((isDirectionRight() && enemy_pos <= pos)
+
+			|| (!isDirectionRight() && pos < enemy_pos + strlen(enemy_shape)))
+
 		{
-			isFired = false;
+
+			resetFire();
+
 		}
-		
+
+
+
 	}
 
-	void draw(char* canvas, int maxCount)
+
+
+	void draw(Screen& screen)
+
 	{
-		if (isFired == false) return;
-		if (isInside(maxCount) == false) return;
-		strncpy(&canvas[pos], shape, strlen(shape));
-	}
-};
 
-struct Player {
-	int		pos;
-	char	shape[100];
+		if (checkFire() == false) return;
+
+		GameObject::draw(screen);
+
+	}
+
+}; // 구조체 Bullet 정의
+
+
+
+struct Player : public GameObject {
+
+
 
 	// constructor 생성자
+
 	Player(const char* shape, int maxCount)
+
+		: GameObject(rand() % (maxCount - strlen(shape)), shape)
+
 	{
-		strcpy(this->shape, shape);
-		this->pos = rand() % (maxCount - strlen(this->shape));
+
 	}
 
-	bool isInside(int length)
-	{
-		return pos <= (length - strlen(shape)) && pos >= 0;
-	}
 
-	void fire(int enemy_pos, Bullet* bullet)
-	{
-		if (bullet == nullptr) return;
-		if (bullet->isFired == true) return;
 
-		bullet->isFired = true;
-		bullet->pos = pos;
+	void fire(int enemy_pos, Bullet& bullet)
+
+	{
+
+		if (bullet.checkFire()) return;
+
+		bullet.setFire();
+
+		int pos = getPos();
+
+		bullet.setPos(pos);
+
 		if (pos < enemy_pos) {
-			bullet->pos += strlen(shape) - 1;
-			strcpy(bullet->shape, "-->");
-			bullet->direction = 0;
+
+			bullet.setPos(bullet.getPos() + (int)strlen(getShape()) - 1);
+
+			bullet.setShape("-->");
+
+			bullet.makeDirectionRight();
+
 		}
+
 		else {
-			strcpy(bullet->shape, "<--");
-			bullet->direction = 1;
+
+			bullet.setShape("<--");
+
+			bullet.makeDirectionLeft();
+
 		}
+
 	}
 
-	void moveRight()
-	{
-		pos++;
-	}
-
-	void moveLeft()
-	{
-		pos--;
-	}
-
-	void draw(char* canvas, int maxCount)
-	{
-		if (isInside(maxCount) == false) return;
-		strncpy(&canvas[pos], shape, strlen(shape));
-	}
 };
+
+
+
+Bullet* findUnusedBullet(Bullet bullets[], int maxBullets)
+
+{
+
+	for (int i = 0; i < maxBullets; i++)
+
+	{
+
+		if (bullets[i].checkFire()) continue;
+
+		return &bullets[i];
+
+	}
+
+	return nullptr;
+
+}
+
 
 
 int main()
-{
-	const int maxCount = 80;
-	char canvas[maxCount + 1];
-	Player	player{ "(o_o)", maxCount };
-	Enemy   enemy{ "(*___*)", maxCount };
-	Bullet	bullet{"->"};
 
-	while (true)		
+{
+
+	Screen screen;
+
+	int maxCount = screen.length();
+
+	Player	player{ "(o_o)", maxCount };
+
+	Enemy   enemy{ "(*___*)", maxCount };
+
+	Bullet* bullets = (Bullet*)malloc(sizeof(Bullet) * maxCount);
+
+
+
+	while (true)
+
 	{
-		clear(canvas, maxCount);
-				
+
+		screen.clear();
+
+
+
 		// update game objects (player, enemy ...)
+
 		if (player.isInside(maxCount) == false || enemy.isInside(maxCount) == false)
+
 			break; // check game loop termination condition
 
+
+
 		if (_kbhit()) {
+
 			int key = _getch();
+
+			Bullet* bullet = nullptr;
+
+
+
 			//printf("\n%c %d\n", key, key);
+
 			switch (key) {
+
 			case 'a':
+
 				player.moveLeft();
+
 				break;
+
 			case 'd':
+
 				player.moveRight();
+
 				break;
+
 			case ' ':
-				player.fire(enemy.pos, &bullet);
+
+				bullet = findUnusedBullet(bullets, maxCount);
+
+				if (bullet == nullptr) break;
+
+				player.fire(enemy.getPos(), *bullet);
+
 				break;
+
 			case 'w':
+
 				enemy.moveRight();
+
 				break;
+
 			case 's':
+
 				enemy.moveLeft();
+
 				break;
+
 			}
+
 		}
-		bullet.update(enemy.pos, enemy.shape);
-		
+
+		for (int i = 0; i < maxCount; i++)
+
+		{
+
+			if (bullets[i].checkFire() == false) continue;
+
+			bullets[i].update(enemy.getPos(), enemy.getShape());
+
+		}
+
+
+
 		// draw game objects to a canvas (player, enemy ...)
-		player.draw(canvas, maxCount);
-		enemy.draw(canvas, maxCount);
-		bullet.draw(canvas, maxCount);		
-		
+
+		player.draw(screen);
+
+		enemy.draw(screen);
+
+		for (int i = 0; i < maxCount; i++)
+
+		{
+
+			if (bullets[i].checkFire() == false) continue;
+
+			bullets[i].draw(screen);
+
+		}
+
+
+
 		// display canvas to a monitor
-		render(canvas, maxCount);		
+
+		screen.render();
+
 		Sleep(100);
+
 	}
+
 	printf("\n정상적으로 종료되었습니다.\n");
+
+
+
+	free((void*)bullets);
+
 	return 0;
+
 }

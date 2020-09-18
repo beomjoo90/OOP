11404657e56ed608bdc439b9ca75b7a84146d957
beomjoo90 <beomjoo90@gmail.com>
2020-09-18 14:53:59 +0900

    콘솔 애플리케이션에서 마우스와 키보드 제어하는 로직 추가하기
    - 콘솔창의 설정에서 'Qick Edit Mode'를 disable 시켜야 합니다. ( https://stackoverflow.com/questions/46795072/how-to-get-mouse-input-inside-a-c-console-program-on-windows-10 )
    -  키보드와 마우스를 제어하는 로직은 ( https://stackoverflow.com/questions/6285270/how-can-i-get-the-mouse-position-in-a-console-program )을 참조하세요.
    - union 키워드 학습
    - MSDN 참조 https://docs.microsoft.com/en-us/windows/console/reading-input-buffer-events?redirectedfrom=MSDN

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -1,9 +1,8 @@
 // main.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
-
+#include <Windows.h>
 #include <iostream>
 #include <conio.h>
-#include <Windows.h>
 #include <ctime>
 #include <cstdlib> 
 #include "Utils.h"
@@ -29,7 +28,7 @@ public:
 
 	static Screen* getInstance() {
 		if (instance == nullptr) {
-			instance = new Screen{ 10,10 };
+			instance = new Screen{ 80, 20 };
 		}
 		return instance;
 	}
@@ -73,10 +72,32 @@ Screen* Screen::instance = nullptr;
 // 싱글턴을 상속할 수 있다.
 
 
+HANDLE hStdin;
+DWORD fdwSaveOldMode;
+
+VOID ErrorExit(const char *);
+VOID KeyEventProc(KEY_EVENT_RECORD);
+VOID MouseEventProc(MOUSE_EVENT_RECORD);
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD);
+
 int main()
 {
 	Screen* screen = Screen::getInstance();
-	
+	DWORD cNumRead, fdwMode;
+	INPUT_RECORD irInBuf[128];
+
+	hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	if (hStdin == INVALID_HANDLE_VALUE)
+		ErrorExit("GetStdHandle");
+
+	if (!GetConsoleMode(hStdin, &fdwSaveOldMode))
+		ErrorExit("GetConsoleMode");
+
+	// Enable the window and mouse input events. 
+
+	fdwMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+	if (!SetConsoleMode(hStdin, fdwMode))
+		ErrorExit("SetConsoleMode");
 
 	bool requestExit = false;
 	int x = 0, y = 0;
@@ -84,18 +105,136 @@ int main()
 	while (requestExit == false)
 	{
 		screen->clear();
+
+		if (!ReadConsoleInput(
+			hStdin,      // input buffer handle 
+			irInBuf,     // buffer to read into 
+			128,         // size of read buffer 
+			&cNumRead)) // number of records read 
+			ErrorExit("ReadConsoleInput");
+
+		// Dispatch the events to the appropriate handler. 
+
+		
+		for (int i = 0; i < cNumRead; i++)
+		{
+			switch (irInBuf[i].EventType)
+			{
+			case KEY_EVENT: // keyboard input 
+				KeyEventProc(irInBuf[i].Event.KeyEvent);
+				break;
+
+			case MOUSE_EVENT: // mouse input 
+				MouseEventProc(irInBuf[i].Event.MouseEvent);
+				break;
+
+			case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing 
+				ResizeEventProc(irInBuf[i].Event.WindowBufferSizeEvent);
+				break;
+
+			case FOCUS_EVENT:  // disregard focus events 
+
+			case MENU_EVENT:   // disregard menu events 
+				break;
+
+			default:
+				ErrorExit("Unknown event type");
+				break;
+			}
+		}
+
+
 		screen->draw(x, y, '0' + x);
 		screen->render();
 
+
+
 		// debugging
-		Borland::gotoxy(20, 20);
-		printf("x = %d, y = %d", x, y);
+		Borland::gotoxy(0, 21);
+		printf("cNumRead = %d", cNumRead);
 
 		Sleep(100);
-		
-		++x %= screen->getWidth();
-		++y %= screen->getHeight();
+
+					
 	}
 	printf("\n정상적으로 종료되었습니다.\n");
 	return 0;
+}
+
+VOID ErrorExit(const char* lpszMessage)
+{
+	Borland::gotoxy(0, 21);
+	printf("%80d\r", ' ');
+	printf("%s\n", lpszMessage);
+
+	// Restore input mode on exit.
+
+	SetConsoleMode(hStdin, fdwSaveOldMode);
+
+	ExitProcess(0);
+}
+
+VOID KeyEventProc(KEY_EVENT_RECORD ker)
+{
+	Borland::gotoxy(0, 22);
+	printf("%80d\r", ' ');
+	
+	printf("Key event: %c  %d        ", ker.uChar, ker.wRepeatCount);
+
+	if (ker.bKeyDown)
+		printf("key pressed\n");
+	else printf("key released\n");
+}
+
+VOID MouseEventProc(MOUSE_EVENT_RECORD mer)
+{
+#ifndef MOUSE_HWHEELED
+#define MOUSE_HWHEELED 0x0008
+#endif
+	Borland::gotoxy(0, 22);
+	printf("%80d\r", ' ');
+
+	printf("Mouse event: %d %d      ", mer.dwMousePosition.X, mer.dwMousePosition.Y);
+
+	switch (mer.dwEventFlags)
+	{
+	case 0:
+
+		if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+		{
+			printf("left button press \n");
+		}
+		else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
+		{
+			printf("right button press \n");
+		}
+		else
+		{
+			printf("button press\n");
+		}
+		break;
+	case DOUBLE_CLICK:
+		printf("double click\n");
+		break;
+	case MOUSE_HWHEELED:
+		printf("horizontal mouse wheel\n");
+		break;
+	case MOUSE_MOVED:
+		printf("mouse moved\n");
+		break;
+	case MOUSE_WHEELED:
+		printf("vertical mouse wheel\n");
+		break;
+	default:
+		printf("unknown\n");
+		break;
+	}
+}
+
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
+{
+	Borland::gotoxy(0, 22);
+	printf("%80d\r", ' ');
+	printf("Resize event:             ");
+	printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
 }
